4ed48e52dd39b3fef09a17d015bfd7fd18e3ce4d
J-Kimani <joshuakimani192@gmail.com>
2025-06-17 22:18:15 +0300

    Music Popularity Prediction

--- R Projects/Music Popularity Prediction/Music Popularity.R
+++ R Projects/Music Popularity Prediction/Music Popularity.R
@@ -13,6 +13,8 @@
 library(dplyr)
 library(ggplot2)
 library(corrplot)
+library(caret)
+library(randomForest)
 
 # import data
 data <- read.csv("songs.csv")
@@ -126,5 +128,87 @@ for (feature in features) {
   print(p)
   }
 
+# Distribution Observations:
+# - Right-skewed features: `duration_ms`, `tempo`, `liveness`, `acousticness`, `speechiness`
+# - Left-skewed features: `loudness`, `energy`, `danceability`
+# - `key` appears uniformly distributed
+# - `valence` shows an approximately normal distribution
 
-  
+# Skewed distributions may benefit from transformation (e.g., log, Box-Cox)
+# to improve model performance and meet assumptions of some regression models.
+
+
+# Feature Selection and Model Training
+
+# Based on correlation and distribution analysis, the following audio features
+# were found to have meaningful relationships with `track_popularity`:
+# - energy, valence, danceability, loudness, acousticness,
+#   tempo, speechiness, and liveness
+#
+# These features reflect key musical attributes that influence listener engagement
+# and can therefore support predictive modeling.
+#
+# Next step:
+# We'll split the data into training and testing sets, scale the features,
+# and train a Random Forest Regression model to predict song popularity.
+rf_features <- c("energy", "valence", "danceability", "loudness", "acousticness", "tempo", "speechiness", "liveness")
+
+X <- reduced[, rf_features]
+y <- reduced$track_popularity
+
+# split the data into training and test size 80/20
+set.seed(42)
+train_index <- createDataPartition(y,p= 0.8, list= FALSE)
+X_train <- X[train_index, ]
+X_test <- X[-train_index, ]
+y_train <- y[train_index]
+y_test <- y[-train_index]
+
+# Normalize the features using preProcess
+scaler <- preProcess(X_train, method = c("center", "scale"))
+X_train_scaled <- predict(scaler, X_train)
+X_test_scaled <- predict(scaler, X_test)
+
+# Define training control and tuning grid for Random Forest
+control <- trainControl(method = "cv", number= 3)
+tune_grid <- expand.grid(
+  .mtry = c(2, 3, 4)
+)
+
+# Train the Random Forest model
+set.seed(42)
+rf_model <- train(
+  x = X_train_scaled,
+  y = y_train,
+  method = "rf",
+  metric = "RMSE",
+  trControl = control,
+  tuneGrid = tune_grid,
+  n_tree = 100
+)
+
+# Best model summary
+print(rf_model$bestTune)
+best_rf_model <- rf_model$finalModel
+
+# Make predictions
+y_pred <- predict(rf_model, newdata =  X_test_scaled)
+head(y_pred)
+
+# Create a data frame for actual vs predicted
+results <- data.frame(
+  Actual = y_test,
+  Predicted = y_pred
+)
+
+# Plot actual vs predicted
+ggplot(results, aes(x = Actual, y = Predicted)) + 
+  geom_point(alpha = 0.7) +
+  geom_abline(intercept = 0, slope = 1, color = "red", size = 1.2)
+  labs(
+    title = "Actual vs Predicted Popularity",
+    x = "Actual Popularity",
+    y = "Predicted Popularity"
+  )+
+theme_minimal()    
+
