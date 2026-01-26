8957641cb94fcea5af48d8d5e0cab63c387bf182
J-Kimani <joshuakimani192@gmail.com>
2026-01-26 22:00:39 +0300

    updates

---
+++ SQL/SQL TUTORIAL.sql
@@ -0,0 +1,70 @@
+DROP DATABASE sql_learn;
+
+CREATE DATABASE sql_learn;
+
+USE sql_learn;
+
+CREATE TABLE emp(
+id INT NOT NULL AUTO_INCREMENT,
+ename VARCHAR(20),
+address VARCHAR(30),
+PRIMARY KEY(id)
+);
+
+DROP TABLE emp;
+
+CREATE TABLE Student(
+StudentID INT NOT NULL AUTO_INCREMENT,
+FName VARCHAR(20),
+LName VARCHAR(20),
+Address VARCHAR(30),
+City VARCHAR(15),
+Marks INT,
+PRIMARY KEY(StudentId)
+);
+
+-- populate the table
+INSERT INTO Student (FName, LName, Address, City, Marks) VALUES
+('John',    'Kamau',   'Ngong Road',        'Nairobi', 78),
+('Mary',    'Achieng', 'Kisumu Central',    'Kisumu',  85),
+('Peter',   'Mwangi',  'Thika Road',        'Thika',   67),
+('Jane',    'Wanjiku', 'Westlands',         'Nairobi', 92),
+('Daniel',  'Otieno',  'Milimani',          'Kisumu',  74),
+('Faith',   'Njeri',   'Karatina Town',     'Karatina',88),
+('Brian',   'Mutua',   'Machakos Junction', 'Machakos',61),
+('Lucy',    'Atieno',  'Busia Road',        'Busia',   70),
+('Samuel',  'Kiptoo',  'Kipchoge Street',   'Eldoret', 81),
+('Grace',   'Chebet',  'Nandi Hills',       'Nandi',   90);
+
+
+
+ -- Select statement
+SELECT * FROM Student;
+
+SELECT Fname, LName FROM Student;
+
+-- Select student from city Nandi
+SELECT FNAME
+FROM Student
+WHERE City = 'Nandi';
+
+-- AND OPERATOR
+SELECT * FROM student
+WHERE Fname = 'Faith' AND Lname= 'Njeri';
+
+-- OR OPERATOR
+SELECT * FROM student
+WHERE Fname = 'Faith' OR Lname= 'Mutua';
+
+-- NOT OPERATOR
+SELECT * FROM Student
+WHERE not Fname = "John";
+
+-- INSERT INTO
+INSERT INTO Student(Fname, Lname, Address, City, Marks)
+VALUES('Joshua', 'Karanja', 'Thika Road', 'Juja', 99);
+
+SELECT * FROM Student;
+
+
+
